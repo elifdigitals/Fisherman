f3dd1163c65aa24480a220d0a26d8dfdda0e59be
theHalf <58950709+ilya04634@users.noreply.github.com>
2025-10-04 08:57:01 +0600

    fixed animation

--- scene/player/player.gd
+++ scene/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-var SPEED = 200
+var SPEED = 60
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,8 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var movement = movement_vector()
-	var direction = movement.normalized()
+	var direction = movement_vector().normalized()
 	velocity = SPEED * direction
 	move_and_slide()
 
@@ -18,9 +17,15 @@ func  movement_vector():
 	var movement_x = Input.get_action_strength("run_right") - Input.get_action_strength("run_left")
 	var movement_y = Input.get_action_strength("run_back") - Input.get_action_strength("run_forward")
 	
+	if movement_x or movement_y != 0:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+	
+	
 	if movement_x > 0:
 		$AnimatedSprite2D.flip_h=false
-	else:
+	if movement_x < 0:
 		$AnimatedSprite2D.flip_h=true
 	
 	
