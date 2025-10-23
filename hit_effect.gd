74f97e22915d11cbf87690a36f34030db6bc3f81
ANITS60106 <104861555+ANITS60106@users.noreply.github.com>
2025-10-23 11:14:04 +0600

    Bug fixes
    Errors исправлен

--- hit_effect.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var anim = $AnimatedSprite2D
-@export var duration := 0.3
-
-func _ready():
-	anim.play("hit")
-	await get_tree().create_timer(duration).timeout
-	queue_free()
